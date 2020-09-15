9644f12a7b03c2518d7738d19e81d345d86eda56
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-09-15 11:21:24 +0900

    #1.0 Layouts with Flexbox in React Native

--- App.js
+++ App.js
@@ -5,7 +5,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello~?</Text>
+      <View style={styles.temp01}><Text style={styles.text}>temp01</Text></View>
+      <View style={styles.temp02}><Text style={styles.text}>temp02</Text></View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,7 +16,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    justifyContent: 'center'
   },
+  temp01: {
+    flex: 1,
+    backgroundColor: 'lightyellow'
+  },
+  temp02: {
+    flex: 3,
+    backgroundColor: 'skyblue'
+  },
+  text: {
+    color: 'black'
+  }
 });
