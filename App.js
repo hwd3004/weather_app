71580153d9068ebde4d88792dffa3ba2dc35b339
hwd3004 <69135198+hwd3004@users.noreply.github.com>
2020-09-15 11:39:19 +0900

    #1.1 Loading Screen

--- App.js
+++ App.js
@@ -1,33 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import Loading from './Loading'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View style={styles.temp01}><Text style={styles.text}>temp01</Text></View>
-      <View style={styles.temp02}><Text style={styles.text}>temp02</Text></View>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <Loading />
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    // alignItems: 'center',
-    justifyContent: 'center'
-  },
-  temp01: {
-    flex: 1,
-    backgroundColor: 'lightyellow'
-  },
-  temp02: {
-    flex: 3,
-    backgroundColor: 'skyblue'
-  },
-  text: {
-    color: 'black'
-  }
-});
